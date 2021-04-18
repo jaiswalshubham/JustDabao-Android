39952de1cc983e66909ad1f8f28363ec639343e9
Shubham <shubhamjaisw1997@gmail.com>
2021-04-19 00:37:05 +0530

    --Check out and confirmation screen

---
+++ app/src/main/java/com/assignment/justdabao/main/CheckInFragment.java
@@ -0,0 +1,22 @@
+package com.assignment.justdabao.main;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.assignment.justdabao.R;
+
+public class CheckInFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_check_in, container, false);
+    }
+}
